c589b884509b48841f87006b20af4ef341ca03a0
Luis Emilio <tdtxle@gmail.com>
2022-11-19 19:03:52 -0600

    nuevo diseño

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 
-import 'Screen/estanteria/estanteria.dart';
-import 'db/models/hive_libreria.dart';
-import 'db/models/hive_libros.dart';
+void main() => runApp(MyApp());
 
-main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await Hive.initFlutter();
-  HiveLibros.open();
-  HiveLibreria.open();
-
-  runApp(MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: EstanteriaScreen(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Material App Bar'),
+        ),
+        body: Center(
+          child: Container(
+            child: Text('Hello World'),
+          ),
+        ),
+      ),
     );
   }
 }
