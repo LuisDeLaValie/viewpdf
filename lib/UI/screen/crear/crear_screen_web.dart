e2b1eb05a475ff4b1c162654c70073d1d3e0e907
Luis Emilio <tdtxle@gmail.com>
2022-12-10 19:34:04 -0600

    crear libro

--- lib/UI/screen/crear/crear_screen_web.dart
+++ lib/UI/screen/crear/crear_screen_web.dart
@@ -1,6 +1,8 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:viewpdf/UI/shared/layout/layout_home.dart';
 import 'package:viewpdf/UI/shared/theme/Colors/ColorA.dart';
+import 'package:viewpdf/model/libros_model.dart';
 
 import 'view/view_formulario.dart';
 
@@ -30,7 +32,15 @@ class CrearScreenWeb extends StatelessWidget {
                       style: ElevatedButton.styleFrom(
                         backgroundColor: ColorA.burntSienna,
                       ),
-                      onPressed: () {},
+                      onPressed: () async {
+                        FilePickerResult? result =
+                            await FilePicker.platform.pickFiles();
+
+                        if (result != null) {
+                          var libros =
+                              LibrosModel.importar(result.files.single.path!);
+                        }
+                      },
                       child: Text("Importar Libor"),
                     ),
                   )
--- lib/provider/crear_libro_provider.dart
+++ lib/provider/crear_libro_provider.dart
@@ -1,9 +1,18 @@
-import 'dart:convert';
 
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+
 
 class CrearLibroProvider with ChangeNotifier {
+  TextEditingController titulo = TextEditingController();
+  // TextEditingController titulo = TextEditingController();
+  // TextEditingController titulo = TextEditingController();
+  // TextEditingController titulo = TextEditingController();
+  // TextEditingController titulo = TextEditingController();
+  // TextEditingController titulo = TextEditingController();
+  // TextEditingController titulo = TextEditingController();
+
   bool _loader = true;
 
   bool get loader => this._loader;
@@ -17,5 +26,35 @@ class CrearLibroProvider with ChangeNotifier {
     notifyListeners();
   }
 
+  void importar() async {
+    FilePickerResult? result;
 
+    try {
+      result = await FilePicker.platform.pickFiles();
+    } catch (e) {
+      print(e);
+    }
+
+    if (result != null) {
+      // Uint8List fileBytes = result.files.first.bytes;
+      String fileName = result.files.first.name;
+      print(fileName);
+    }
+
+    // FilePickerResult? result;
+    // try {
+    //   result = await FilePicker.platform.pickFiles(
+    //     type: FileType.custom,
+    //     allowedExtensions: ['pdf', 'doc'],
+    //   );
+    // } catch (e) {
+    //   log(e.toString());
+    // }
+
+    // if (result != null) {
+    //   File file = File.fromRawPath(result.files.first.bytes!);
+    //   var libros = LibrosModel.importar(file.path);
+    //   notifyListeners();
+    // }
+  }
 }
